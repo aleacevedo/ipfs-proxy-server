07b7c99156b50bde45955e6f68945e9d4b1e288d
aleacevedo <ale.acevedo@live.com.ar>
2021-03-24 20:50:26 -0300

    feat: ignore build/

--- proxy-server/build/controllers/users.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.authenticate = void 0;
-const authenticate = async (req, res) => {
-    return res.status(200).send();
-};
-exports.authenticate = authenticate;
