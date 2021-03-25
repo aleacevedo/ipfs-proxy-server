4601ca8bebcf0a106f3dcf081932a87eb210f0ff
aleacevedo <ale.acevedo@live.com.ar>
2021-03-24 22:22:43 -0300

    feat: add user to request attributes and implement controller

--- server/src/controllers/api-keys.ts
+++ server/src/controllers/api-keys.ts
@@ -1,10 +1,17 @@
 import { RequestHandler } from "express";
 
-export const create: RequestHandler = async (req, res) => {
+import { ApiKey, User } from "models";
+
+export const create: RequestHandler = async ({ user, ...req }, res) => {
+  if (!user) return res.status(400).send();
+  ApiKey.query().insert({ userId: user.id });
   return res.status(201).send();
 };
 
-export const modify: RequestHandler = async (req, res) => {
+export const modify: RequestHandler = async ({ user, ...req }, res) => {
+  if (!user) return res.status(400).send();
+  const apiKey = ApiKey.query().where({ id: req.query.id, userId: user.id });
+  apiKey.patch(req.params);
   return res.status(200).send();
 };
 
--- server/src/routes/api-keys-routes.ts
+++ server/src/routes/api-keys-routes.ts
@@ -5,7 +5,7 @@ import { create, modify, logs } from "../controllers/api-keys";
 const router = express.Router();
 
 router.post("/", create);
-router.put("/", modify);
+router.put("/:id", modify);
 router.get("/:id/logs", logs);
 
 export default router;
